6c7932a17b7ebe23afc0ff2c58d577acebaa2cb3
RudenkoAD <rudenko.ad@phystech.edu>
2023-09-16 00:51:59 +0300

    [UPDATE] modified vk post parser to correctly handle embedded links that weren't from the vk domain

--- bot/vk_post_parser.py
+++ bot/vk_post_parser.py
@@ -52,32 +52,30 @@ def get_message_texts(group_name, post: dict):
   return text_list
 
 def parse_vk_post_text(post_text):
-    # Regular expression to match VK embeds (both "id" and "club")
-    embed_pattern = r'\[(id|club)(\d+)\|([^\]]+)\]'
+    # Regular expression to match VK embeds (both "id", "club", and standard links)
+    embed_pattern = r'\[(id|club)(\d+)\|([^\]]+)\]|\[([^|\]]+)\|([^]]+)\]'
 
     def replace_embed(match):
-        embed_type = match.group(1)
-        entity_id = match.group(2)
-        display_name = escape(match.group(3))
-        
-        if embed_type == 'id':
-            vk_link = f'<a href="https://vk.com/id{entity_id}">{display_name}</a>'
-        elif embed_type == 'club':
-            vk_link = f'<a href="https://vk.com/club{entity_id}">{display_name}</a>'
-        else:
-            vk_link = match.group(0)  # Return the original text if neither "id" nor "club"
-        
+        if match.group(1) is not None:  # "id" or "club" format
+            embed_type = match.group(1)
+            entity_id = match.group(2)
+            display_name = escape(match.group(3))
+
+            if embed_type == 'id':
+                vk_link = f'<a href="https://vk.com/id{entity_id}">{display_name}</a>'
+            elif embed_type == 'club':
+                vk_link = f'<a href="https://vk.com/club{entity_id}">{display_name}</a>'
+        else:  # Standard link format
+            entity_id = match.group(4)
+            display_name = escape(match.group(5))
+            vk_link = f'<a href="{entity_id}">{display_name}</a>'
+
         return vk_link
 
-    # Replace VK embeds with Telegram-compatible HTML hyperlinks
-    parsed_text = re.sub(embed_pattern, replace_embed, post_text)
-
-    return parsed_text
+    return re.sub(embed_pattern, replace_embed, post_text)
 
 # Example usage:
 if __name__ == "__main__":
-  vk_post_text = """Приветствуем нового старосту третьекурсников — [id426956952|Савву Савенкова]! 
-  Напомним, что староста курса — это тот, кто представляет интересы всего потока. Теперь Савва доступен для решения любых организационных, но не только, вопросов. Он следит за работой старост всех групп и передает информацию от преподавателей.
-  Желаем успехов Савве!"""
+  vk_post_text = "Check out [id123|John] and [club456|My Club] on VK. Also, visit [https://example.com|our website]."
   parsed_text = parse_vk_post_text(vk_post_text)
   print(parsed_text)
